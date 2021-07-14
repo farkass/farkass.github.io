d694d80623b44d1b661e8719647b8a3dc2ff7740
farkass <farkas.sandor1@outlook.hu>
2021-07-14 10:54:47 +0200

    Delete phpcheck.php

--- phpcheck.php
+++
@@ -1,9 +0,0 @@
-Congratulations, your server can process PHP!
-"; print "
-
-Here you can view the PHP configuration on your server
-"; phpinfo( ); exit(); ?> 
-
-Sorry, PHP is not enabled on this server.
-
-Check your host provider documentation or contact your host provider to ask them how to enable PHP.
